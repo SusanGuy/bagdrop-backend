18569e19945e423d3ee0960a0eccd56d6f99cb7e
Susan Subedi <susansubedi34@gmail.com>
2019-10-08 12:07:28 -0500

    Allowed Addition Field for signup

--- passport/SignupStrategy.js
+++ passport/SignupStrategy.js
@@ -3,7 +3,8 @@ const bcrypt=require('bcrypt');
 const saltRound=10;
 const connection = require('../connection.js');
 
-const SignupStrategy= new Strategy({passReqtoCallback:true},(username,password,done)=>{
+const SignupStrategy= new Strategy({passReqToCallback:true},(req,username,password,done)=>{
+
     connection.query("SELECT * FROM users WHERE username = ? ",
     [username], (err, rows)=>{
         if(err){
@@ -14,10 +15,18 @@ const SignupStrategy= new Strategy({passReqtoCallback:true},(username,password,d
         }
         let newUser ={
         username,
-        password: bcrypt.hashSync(password, saltRound)
+        firstname:req.body.fname,
+        lastname:req.body.lname,
+        email:req.body.email,
+        email1:req.body.email1,
+        password: bcrypt.hashSync(password, saltRound),
+        type:req.body.type,
+
+
         };
-        let insertQuery = "INSERT INTO users (username, password) values (?, ?)";
-        connection.query(insertQuery, [newUser.username, newUser.password],
+         console.log(req.body.fname)
+        let insertQuery = "INSERT INTO users (username,first_name,last_name,email,email_confirmed,password,user_type) values (?,?,?,?,?,?,?)";
+        connection.query(insertQuery, [newUser.username, newUser.firstname, newUser.lastname, newUser.email, newUser.email1, newUser.password,newUser.type],
             (err, rows)=>{
                 if(err){
                     return done(err,null);
